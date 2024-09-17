6af470c326a08487e1b786ab54d48b6737240ecb
Cyrine Fk <sirinefakhfakh03@gmail.com>
2024-09-17 18:49:03 +0100

    app

--- model/main.py
+++ model/main.py
@@ -3,7 +3,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
-#import pickle5 as pickle
+import pickle5 as pickle
 
 
 def create_model(data): 
@@ -46,11 +46,11 @@ def main():
 
   model, scaler = create_model(data)
 
- # with open('model/model.pkl', 'wb') as f:
-    #pickle.dump(model, f)
+  with open('model/model.pkl', 'wb') as f:
+    pickle.dump(model, f)
     
-  #with open('model/scaler.pkl', 'wb') as f:
-    #pickle.dump(scaler, f)
+  with open('model/scaler.pkl', 'wb') as f:
+    pickle.dump(scaler, f)
   
 
 if __name__ == '__main__':
